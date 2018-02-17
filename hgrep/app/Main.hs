8304abf5f607947ea49021ef0b0da1b104c88f00
Amir <amir.sahebfosoul@gmail.com>
2018-02-17 22:53:08 +0100

    a really bad, dirty, full of errors piece of code!

--- hgrep/app/Main.hs
+++ hgrep/app/Main.hs
@@ -1,15 +1,48 @@
 module Main where
 
 import System.Environment
+import Grammar
 
+-- reads the file, send the lines to checkLine
 readWords arg = do
  f <- readFile arg
- let w = words f
+ let l = lines f
+ checkLine l
+ -- let m = map words l
+ -- let w = Grammar.match . (map words l)
  -- let count = show (length w)
  -- putStr count
- printWords w
+ -- map Grammar.match w regex
+ -- printWords w
+ -- printWords l
+ -- mapM printWords m
 
+-- gets the list of lines of a file and check if the contents match or not
+-- checkLine [] regex = putStr ""
+checkLine [x] regex = do 
+    let ws = words x
+    let isMatched = orList (matchLine ws)
+    -- let isMatched = map (Grammar.match (words x)) (Grammar.str2regex regex)
+    -- let result = orList isMatched
+    if (isMatched) then putStrLn x else putStr ""
+checkLine (x:xs) regex = checkLine x
 
+-- gets a single line and check the matching situation
+matchLine [] regex = False
+matchLine [x] regex = Grammar.match x (Grammar.str2regex regex)
+matchLine (x:xs) regex = matchLine x regex ++ matchLine xs regexs
+-- wordsOfLine [] = putStr ""
+-- wordsOfLine x = do
+--     let ws = words x
+--     let strRegex = Grammar.str2regex regex
+--     let foundList = 
+
+-- gets a list of bools and do the OR between the elements
+orList [] = False
+orList [x] = x
+orList (x:xs) = x || orList xs
+
+-- gets the list of Strings and prints them
 printWords [] = putStr ""
 printWords [x] = putStrLn x
 printWords (x:xs) = do 
