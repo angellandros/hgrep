05fb5ea7cc4a035f222e0656642c7e51bc1f2a2b
Muhammad-Ali A'rabi <angellandros@yahoo.com>
2018-02-18 19:33:39 +0100

    Unit tests

--- hgrep/README.md
+++ hgrep/README.md
@@ -9,9 +9,19 @@ $ stack build
 ```
 Now running the binary is possible through `stack exec`:
 ```
-$ stack exec hrep-exe regex FILE1 [FILE2 [...]]
+$ stack exec hgrep-exe regex FILE1 [FILE2 [...]]
 ```
 
 ## Usage
 Valid regex operations are `()*·|`, ordered by priority. Interpunct `·` (`'\183'` in Haskell) can be omitted, but by the way is more prior than `*`, e.g.
 `"a·(b)*"` = `"a·b*"` = `"ab*"` ≠ `"(ab)*"`.
+
+```
+$ stack exec hgrep-exe "m(a|i|n)*" app/Main.hs test/Spec.hs
+```
+
+## Test
+One can run the tests via:
+```
+$ stack test
+```
--- hgrep/test/Spec.hs
+++ hgrep/test/Spec.hs
@@ -1,2 +1,29 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+
+import Test.HUnit
+import Grammar
+
+-- main :: IO ()
+main = runTestTT testList
+
+testNull1 = TestCase $ assertEqual "nullable" (nullable RegExEmpty) False
+testNull2 = TestCase $ assertEqual "nullable" (nullable (RegExAtom (AtomEpsilon (GEpsilonC)))) True
+testNull3 = TestCase $ assertEqual "nullable" (nullable (RegExAtom (AtomLit (GLitC 'a')))) False
+
+testRParse1 = TestCase $ assertEqual "str2regex" (str2regex "c") (RegExAtom (AtomLit (GLitC 'c')))
+
+testMatch1 = TestCase $ assertEqual "match" (match "b" (str2regex "a|b")) True
+testMatch2 = TestCase $ assertEqual "match" (match "abb" (str2regex "(a|b)*")) True
+testMatch3 = TestCase $ assertEqual "match" (match "a" (str2regex "ab*")) True
+testMatch4 = TestCase $ assertEqual "match" (match "abbb" (str2regex "ab*")) True
+testMatch5 = TestCase $ assertEqual "match" (match "b" (str2regex "a*")) False
+
+testList = TestList [
+    TestLabel "nullable nothing = False" testNull1, 
+    TestLabel "nullable epsilon = True" testNull2,
+    TestLabel "nullable literal = False" testNull3,
+    TestLabel "str2regex 'a' = atom 'a'" testRParse1,
+    TestLabel "b ~ a|b" testMatch1,
+    TestLabel "abb ~ (a|b)*" testMatch2,
+    TestLabel "a ~ ab*" testMatch3,
+    TestLabel "abbb ~ ab*" testMatch4,
+    TestLabel "b /~ a*" testMatch5]
