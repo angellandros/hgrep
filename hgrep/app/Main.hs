a5701309e5ef10a2c42ce3174ea880c3e2a18234
Amir <amir.sahebfosoul@gmail.com>
2018-02-18 17:35:29 +0100

    highlighting feature added

--- hgrep/app/Main.hs
+++ hgrep/app/Main.hs
@@ -2,11 +2,16 @@ module Main where
 
 import System.Environment
 import Grammar
+import Data.Char
+import Data.List
+import Data.Maybe
 
 -- gets a single line and check the matching situation
-matchLine :: [String] -> String -> Bool
-matchLine [] _ = False
-matchLine (w:ws) regexstr = Grammar.match w (Grammar.str2regex regexstr) || matchLine ws regexstr
+matchLine :: [String] -> String -> String
+matchLine [] _ = []
+matchLine (w:ws) regexstr 
+    | Grammar.match w (Grammar.str2regex regexstr) = w
+    | otherwise = matchLine ws regexstr
 
 -- foreach abstraction
 foreach f [] = return ()
@@ -22,9 +27,17 @@ matchFileLines regexstr path = do
 
 -- check the line words with the regex (using matchLine) and then print it when match
 checkLineAndPrint regexstr line = 
-    if matchLine (words line) regexstr then
-        putStrLn line
+    if (matchLine (words line) regexstr) /= [] then
+        let w = matchLine (words line) regexstr in
+        let i = fromJust (findSubstring w line) in
+        putStrLn $ ((slice 0 (i-1) line) ++ "\x1b[32m" ++ (slice i (i + (length w)) line) ++ "\x1b[0m" ++ (slice (i + (length w)+1) (i+(length line)) line))
     else return ()
+
+
+findSubstring :: Eq a => [a] -> [a] -> Maybe Int
+findSubstring pat str = findIndex (isPrefixOf pat) (tails str) 
+
+slice start end = take (end - start + 1) . drop start
 
 -- main function
 main = do 
