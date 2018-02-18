de5a0d6d00aa826a5bb3d0b3bfa6fb3ab1b53e98
Muhammad-Ali A'rabi <angellandros@yahoo.com>
2018-02-18 10:58:05 +0100

    Make it work

--- hgrep/app/Main.hs
+++ hgrep/app/Main.hs
@@ -3,56 +3,31 @@ module Main where
 import System.Environment
 import Grammar
 
--- reads the file, send the lines to checkLine
-readWords arg = do
- f <- readFile arg
- let l = lines f
- checkLine l
- -- let m = map words l
- -- let w = Grammar.match . (map words l)
- -- let count = show (length w)
- -- putStr count
- -- map Grammar.match w regex
- -- printWords w
- -- printWords l
- -- mapM printWords m
-
--- gets the list of lines of a file and check if the contents match or not
--- checkLine [] regex = putStr ""
-checkLine [x] regex = do 
-    let ws = words x
-    let isMatched = orList (matchLine ws)
-    -- let isMatched = map (Grammar.match (words x)) (Grammar.str2regex regex)
-    -- let result = orList isMatched
-    if (isMatched) then putStrLn x else putStr ""
-checkLine (x:xs) regex = checkLine x
-
 -- gets a single line and check the matching situation
-matchLine [] regex = False
-matchLine [x] regex = Grammar.match x (Grammar.str2regex regex)
-matchLine (x:xs) regex = matchLine x regex ++ matchLine xs regexs
--- wordsOfLine [] = putStr ""
--- wordsOfLine x = do
---     let ws = words x
---     let strRegex = Grammar.str2regex regex
---     let foundList = 
+matchLine :: [String] -> String -> Bool
+matchLine [] _ = False
+matchLine (w:ws) regexstr = Grammar.match w (Grammar.str2regex regexstr) || matchLine ws regexstr
 
--- gets a list of bools and do the OR between the elements
-orList [] = False
-orList [x] = x
-orList (x:xs) = x || orList xs
+-- foreach abstraction
+foreach f [] = return ()
+foreach f (x:xs) = do
+    f x
+    foreach f xs
 
--- gets the list of Strings and prints them
-printWords [] = putStr ""
-printWords [x] = putStrLn x
-printWords (x:xs) = do 
-    let element = x
-    putStrLn element
-    printWords xs
+-- get a file path and pass the lines to checkLineAndPrint
+matchFileLines regexstr path = do
+    content <- readFile path
+    foreach (checkLineAndPrint regexstr) (lines content)
+    return ()
 
+-- check the line words with the regex (using matchLine) and then print it when match
+checkLineAndPrint regexstr line = 
+    if matchLine (words line) regexstr then
+        putStrLn line
+    else return ()
 
+-- main function
 main = do 
-    (regex:files) <- getArgs
+    args <- getArgs    
+    foreach (matchFileLines (head args)) (drop 1 args)
 
-    putStrLn regex -- just to show the first arg
-    mapM readWords files
